ee0c44cfe04c2bd114b8938c5a48b0558d4a08ed
DimaOsinnii <ozerland@gmail.com>
2020-12-09 22:47:48 +0200

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,34 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import Logo from './logo.jpg'
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+import printMe from "./print";
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    const myLogo = new Image();
-    myLogo.src = Logo;
-
-    element.appendChild(myLogo);
-
-    console.log(Data);
-    console.log(Notes);
+    element.appendChild(btn);
 
     return element;
 }
